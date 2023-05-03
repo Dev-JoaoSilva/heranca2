8ff0024bc2c47802e02f63a7cba68f90dd3b20d0
João Gabriel <joao.silva2714@etec.sp.gov.br>
2023-05-03 17:10:35 +0000

    saifqpfq

--- Pessoa.java
+++ Pessoa.java
@@ -1,5 +1,35 @@
 public class Pessoa{
     
+
+    public static void main(String[] args) {
+        
+        Pessoa pessoa =new Pessoa("Isaque",24,"Etec");
+
+        System.out.println(pessoa.getNome());
+        System.out.println(pessoa.getIdade());
+        System.out.println(pessoa.getEndereço());
+
+        Funcionario funcionario = new Funcionario("Isaque", 24, "Etec", "Professor");
+
+        System.out.println(funcionario.getNome());
+        System.out.println(funcionario.getMarca());
+        System.out.println(funcionario.getCor());
+        System.out.println(funcionario.getAno());
+
+        Patinete patinete =new Patinete("Bandeirantes","Bandeirantes","Rosa", 2023);
+
+        System.out.println(patinete.getNome());
+        System.out.println(patinete.getMarca());
+        System.out.println(patinete.getCor());
+        System.out.println(patinete.getAno());
+
+
+
+
+
+
+
+
     private String nome;
     private int idade;
     private String endereço;
